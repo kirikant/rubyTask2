e4b847b209d79ef34f1cbf8a3a0c08cab27524ba
kirikant <kirikant@yandex.ru>
2022-07-11 13:20:59 +0200

    add_curb_and_refactor

--- convertor.rb
+++ convertor.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'curb'
 require 'csv'
 require 'nokogiri/class_resolver'
 require 'nokogiri'
@@ -13,8 +14,7 @@ class Convertor
 
   private_class_method :new
 
-  def initialize
-  end
+  def initialize; end
 
   def self.instance
     return @instance if @instance
@@ -28,33 +28,32 @@ class Convertor
 
   def get_category_info(url, file_name)
     page_counter = 1
-
-    while true do
+    marker = true
+    while marker
       threads = []
-      html = URI.open(url + "&p=#{page_counter}")
-      base = html.base_uri
+      html = if page_counter == 1
+               Curl.get(url)
+             else
+               Curl.get(url + "&p=#{page_counter}")
+             end
 
-      current_url = Addressable::URI.parse(url + "&p=#{page_counter}")
-      category_page = Nokogiri::HTML(html)
+      if !String.new(html.body_str).empty?
 
-      if current_url.to_s.split("p=")[1].eql?(base.to_s.split("p=")[1]) || page_counter == 1
-
+        category_page = Nokogiri::HTML(html.body_str)
         puts "parsing page number #{page_counter}"
         products = category_page.xpath('//div[@class="main_content_area"]//div[@class="columns-container wide_container"]
     //div[@class="pro_outer_box"]')
 
         products_hash = get_href_title(products)
         products_packages = get_packages_prices(products_hash)
-        combine_info(products_hash, products_packages, "#{file_name}", page_counter)
+        combine_info(products_hash, products_packages, file_name.to_s, page_counter)
 
-        page_counter = page_counter + 1
+        page_counter += 1
 
       else
-        break
+        marker = false
       end
-
     end
-
   end
 
   private
@@ -63,69 +62,66 @@ class Convertor
     threads = []
     package_refs = []
 
-    titles_hash = Hash.new
-    image_refs_hash = Hash.new
+    titles_hash = {}
+    image_refs_hash = {}
 
     products_info = products.xpath('//a[@class=
 "product_img_link pro_img_hover_scale product-list-category-img"]')
 
-    puts "getting links of products packages"
+    puts 'getting links of products packages'
 
-    products_info.xpath('@href').each {
-      |href|
+    products_info.xpath('@href').each do |href|
       package_refs << href
-    }
+    end
 
-    puts "getting titles of products"
+    puts 'getting titles of products'
     threads << Thread.new do
-      products_info.xpath('@title').each_with_index {
-        |title, i|
+      products_info.xpath('@title').each_with_index do |title, i|
         titles_hash[String.new(package_refs[i])] = String.new(title)
-      }
+      end
     end
 
-    puts "getting images of products"
+    puts 'getting images of products'
     threads << Thread.new do
-      products_info.xpath('//img[@class="replace-2x img-responsive front-image"]//@src').each_with_index {
-        |image_ref, i|
+      products_info.xpath('//img[@class="replace-2x img-responsive front-image"]//@src').each_with_index do |image_ref, i|
         image_refs_hash[String.new(package_refs[i])] = String.new(image_ref)
-      }
+      end
     end
-    threads.each { |t| t.join }
+    threads.each(&:join)
 
     [titles_hash, image_refs_hash, package_refs]
   end
 
   def get_packages_prices(products_hash)
-    puts "getting prices of products"
+    puts 'getting prices of products'
     threads = []
-    ref_packages = Hash.new
-    products_hash[2].each { |ref| threads << Thread.new {
-      html = URI.open(ref)
-      product_page = Nokogiri::HTML(html)
-      html.close
+    ref_packages = {}
+    products_hash[2].each do |ref|
+      threads << Thread.new do
+        html = Curl.get(ref)
+        product_page = Nokogiri::HTML(html.body_str)
+        html.close
 
-      (product_page.xpath('//div[@class="main_content_area"]
-       //div[@class="columns-container wide_container"]')).each { |packages|
+        product_page.xpath('//div[@class="main_content_area"]
+         //div[@class="columns-container wide_container"]').each do |packages|
 
-        package_types = []
-        package_prices = []
+          package_types = []
+          package_prices = []
 
-        packages.xpath('//span[@class="radio_label"]').each { |package|
-          package_types << String.new(package)
-        }
-        packages.xpath('//span[@class="price_comb"]').each { |price|
-          package_prices << String.new(price)
-        }
+          packages.xpath('//span[@class="radio_label"]').each do |package|
+            package_types << String.new(package)
+          end
+          packages.xpath('//span[@class="price_comb"]').each do |price|
+            package_prices << String.new(price)
+          end
 
-        package_types.each_with_index { |package, i|
-          ref_packages["#{package},#{package_prices[i]}"] = String.new(ref)
-        }
-      }
-    }
-    }
-    threads.each { |t| t.join }
+          package_types.each_with_index do |package, i|
+            ref_packages["#{package},#{package_prices[i]}"] = String.new(ref)
+          end
+        end
+      end
+    end
+    threads.each(&:join)
     ref_packages
   end
-
 end
